0a8a76c247a7fe5fd0cc1ec6ea5477105b3509f8
Alekh Kumar <alekhkumar777@gmail.com>
2021-10-07 00:02:10 +0530

    Add login feature

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,4 +1,24 @@
 $(()=>{
-    $('#navbar').load('/components/navbar.html')
+    $('#navbar').load('/components/navbar.html', loginIfNeeded)
     $('#footer').load('/components/footer.html')
+
 })
+
+function loginIfNeeded(){
+    window.currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
+    console.log(currentUser)
+
+    if(!currentUser){
+        $.post('/api/users', {}, (user)=>{
+            if(user){
+                console.log('Registered current user as ', user.username)
+                window.localStorage.user = JSON.stringify(user)
+                currentUser = user
+                $('#nav-username').text(currentUser.username)
+            }
+        })
+    } else {
+        console.log('Resuming session as ', currentUser.username)
+        $('#nav-username').text(currentUser.username)
+    }
+}
